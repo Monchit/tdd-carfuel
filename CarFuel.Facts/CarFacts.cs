4419c076adc6318371f952d4f8452d4a1c664692
Monchit <moomonchit1985@hotmail.com>
2015-07-10 14:06:53 +0700

    Add car and support forgot fillup
    Car can has several fillups. You can get average km/l of a car.
    Fillup also support to flag that user had forget to record previously fillup.
    
    Fixed #1 and Solved #2

---
+++ CarFuel.Facts/CarFacts.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xunit; // (2)
+using CarFuel.Models; 
+
+namespace CarFuel.Facts
+{
+    public class CarFacts // * Add public (1)
+    {
+        public class GeneralUsage
+        {
+            [Fact]
+            public void NewCar_HasCorrectInitialValues()
+            {
+                var c = new Car();
+
+                c.Make = "Honda";
+                c.Model = "Accord";
+
+                Assert.Equal("Honda", c.Make);
+                Assert.Equal("Accord", c.Model);
+                Assert.Equal(0, c.FillUps.Count());
+            }
+        }
+
+        public class AddFillUpMethod
+        {
+            [Fact]
+            public void CanAddNewFillUp()
+            {
+                var c = new Car();
+
+                FillUp f = c.AddFillUp(odometer: 1000, liters: 50.0, isFull: true);
+
+                //Check Method FillUps is OK
+                Assert.Equal(1, c.FillUps.Count());
+
+                //Check Return value
+                Assert.Equal(1000, f.Odometer);
+                Assert.Equal(50.0, f.Liters);
+                Assert.True(f.IsFull);
+            }
+
+            [Fact]
+            public void AddTwoFillUps()
+            {
+                var c = new Car();
+
+                // add f1
+                FillUp f1 = c.AddFillUp(odometer: 500, liters: 50.0, isFull: true);
+                // add f2
+                FillUp f2 = c.AddFillUp(odometer: 1000, liters: 20.0, isFull: true);
+
+                //Assert that c has 2 file ups.
+                Assert.Equal(2, c.FillUps.Count());
+                //Assert that f1.NextFillUp is f2 -> use .same
+                Assert.Same(f2, f1.NextFillUp);
+                Assert.Same(f1, f2.PreviousFillUp);
+            }
+
+            [Fact]
+            public void AddThreeFillUps()
+            {
+                var c = new Car();
+
+                // add f1
+                FillUp f1 = c.AddFillUp(odometer: 500, liters: 50.0, isFull: true);
+                // add f2
+                FillUp f2 = c.AddFillUp(odometer: 1000, liters: 20.0, isFull: true);
+                // add f3
+                FillUp f3 = c.AddFillUp(odometer: 800, liters: 30.0, isFull: true);
+
+                //Assert that c has 3 file ups.
+                Assert.Equal(3, c.FillUps.Count());
+                //Assert that f1.NextFillUp is f2 -> use .Same
+                Assert.Same(f1.NextFillUp, f2);
+                //Assert that f2.NextFillUp is f3 -> use .Same
+                Assert.Same(f2.NextFillUp, f3);
+            }
+
+            
+        }
+
+        public class AverageKilometersPerLiterProperty
+        {
+            [Fact]
+            public void NoFillUp_IsNull()
+            {
+                var c = new Car();
+
+                Assert.Null(c.AverageKilometersPerListes);
+            }
+
+            [Fact]
+            public void SingleFillUp_IsNull()
+            {
+                var c = new Car();
+                FillUp f1 = c.AddFillUp(odometer: 1000, liters: 50.0, isFull: true);
+
+                Assert.Null(c.AverageKilometersPerListes);
+            }
+
+            [Fact]
+            public void TwoFillUps()
+            {
+                var c = new Car();
+
+                FillUp f1 = c.AddFillUp(odometer: 1000,
+                                       liters: 50.0,
+                                       isFull: true);
+
+                FillUp f2 = c.AddFillUp(odometer: 1600,
+                                       liters: 60.0,
+                                       isFull: true);
+
+                Assert.Equal(10.0, c.AverageKilometersPerListes);
+            }
+
+            [Fact]
+            public void ThreeFillUps()
+            {
+                var c = new Car();
+
+                FillUp f1 = c.AddFillUp(odometer: 1000,
+                                       liters: 50.0,
+                                       isFull: true);
+
+                FillUp f2 = c.AddFillUp(odometer: 1600,
+                                       liters: 60.0,
+                                       isFull: true);
+
+                FillUp f3 = c.AddFillUp(odometer: 2000,
+                                       liters: 50.0,
+                                       isFull: true);
+
+                Assert.Equal(9.1, c.AverageKilometersPerListes);
+            }
+
+            [Fact]
+            public void CanAvgKmPerLiter()
+            {
+                var c = new Car();
+
+                // add f1
+                FillUp f1 = c.AddFillUp(odometer: 1000, liters: 50.0, isFull: true);
+                // add f2
+                FillUp f2 = c.AddFillUp(odometer: 1600, liters: 60.0, isFull: true);
+                // add f3
+                FillUp f3 = c.AddFillUp(odometer: 2000, liters: 50.0, isFull: true);
+
+                double? kml = c.AverageKilometersPerListes;
+
+                Assert.Equal(9.1, kml);
+            }
+        }
+
+        public class AverageKml_ForgotFillUps
+        {
+            [Fact]
+            public void OneBlock()
+            {
+                var c = new Car();
+
+                c.AddFillUp(odometer: 1000, liters: 50.0, isFull: true);
+                c.AddFillUp(odometer: 1600, liters: 60.0, isFull: true, isForgot: true);
+                c.AddFillUp(odometer: 2000, liters: 50.0, isFull: true);
+                c.AddFillUp(odometer: 2600, liters: 60.0, isFull: true);
+                c.AddFillUp(odometer: 3500, liters: 50.0, isFull: true, isForgot: true);
+
+                double? kml = c.AverageKilometersPerListes;
+
+                Assert.Equal(9.1, kml);
+            }
+
+            [Fact]
+            public void TwoBlock()
+            {
+                var c = new Car();
+
+                c.AddFillUp(1000, 50);
+                c.AddFillUp(1600, 60);
+                c.AddFillUp(2000, 50);
+
+                c.AddFillUp(4000, 50, isForgot: true);
+                c.AddFillUp(4600, 50);
+                c.AddFillUp(5000, 50);
+
+                var kml = c.AverageKilometersPerListes;
+
+                Assert.Equal(9.6, kml);
+            }
+        }
+    }
+}
---
+++ CarFuel.Models/Car.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CarFuel.Models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public virtual ICollection<FillUp> FillUps { get; set; }
+
+        public Car()
+        {
+            FillUps = new HashSet<FillUp>();
+        }
+
+        public FillUp AddFillUp(int odometer, double liters, bool isFull = true, bool isForgot = false)
+        {
+            //throw new NotImplementedException();
+            var f = new FillUp();
+            f.Odometer = odometer;
+            f.Liters = liters;
+            f.IsFull = isFull;
+            f.IsForgot = isForgot;
+
+            var prev = this.FillUps.LastOrDefault();
+            if (prev != null)
+            {
+                prev.NextFillUp = f;
+                f.PreviousFillUp = prev;
+            }
+
+            this.FillUps.Add(f);
+
+            return f;
+        }
+
+        public double? AverageKilometersPerListes
+        {
+            get
+            {
+                if (FillUps.Count() < 2)
+                {
+                    return null;
+                }
+
+                
+                FillUp first;
+                FillUp last = FillUps.Last();
+                double sumKML = 0.0;
+                int blocks = 0;
+                do
+                {
+                    while (last.IsForgot && last.PreviousFillUp != null)
+                    {
+                        last = last.PreviousFillUp;
+                    }
+
+                    first = last;
+                    var liters = 0.0;
+                    while (first.PreviousFillUp != null)
+                    {
+                        liters += first.Liters;
+                        first = first.PreviousFillUp;
+                        if (first.IsForgot) break;
+                    }
+
+                    // all distance / all lites (excepts the first)
+                    var distance = last.Odometer - first.Odometer;
+
+                    if (liters > 0)
+                    {
+                        var kml = Math.Round(distance / liters, 1, MidpointRounding.AwayFromZero);
+                        sumKML += kml;
+                        blocks++;
+                    }
+
+                    last = first.PreviousFillUp;
+
+                } while (last != null);
+
+                return Math.Round(sumKML / blocks, 1, MidpointRounding.AwayFromZero);
+
+            }
+        }
+    }
+}
